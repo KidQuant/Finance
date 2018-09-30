e19f6736c0e3ee7bac0147e5b9ac1693330152c9
Sealy <as70906n@pace.edu>
2018-09-30 15:35:20 -0400

    For loop added for scaling and labeling

--- trading_ml.py
+++ trading_ml.py
@@ -26,7 +26,7 @@ df['close'] = df['close_price'].shift(1)
 df['volume'] = df['volume'].shift(1)
 
 X = df[['open', 'high', 'low', 'close']]
-y = df['close']
+y = df['close_price']
 
 imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
 
@@ -40,3 +40,23 @@ parameters = {'lasso_alpha':np.arange(0.0001, 10,.0001),
               'lasso_max_iter':np.random.uniform(100,100000,4)}
 
 reg = rcv(pipeline, parameters, cv=5)
+
+for t in np.arange(50,97,3):
+    get_ipython().magic('reset_selective -f reg1')
+    split = int(t*len(X)/100)
+    reg.fit(X[:split],y[:split])
+    best_alpha = reg.best_params_['lasso_alpha']
+    best_iter = reg.best_paras_['lasso_max_iter']
+    reg1 = Lasso(alpha=best_alpha, max_iter=best_iter)
+    X = imp.fit_transform(X,y)
+    reg1.fit(X[:split],y[:split])
+
+    df['P_C_%i'%t] =0.
+    df.iloc[:df.columns.get_loc('P_C_%'%t)]=regl.predict(x[:])
+    df['Error_%i'%t] = np.abs(df['P_C_%i'%t] - df['close'])
+
+    e = np.mean(df['Error_%i'&t][split:])
+    train_e = np.mean(df['Error_%i'%t][:split])
+    avg_err[t] = e
+    avg_train_err[t] = train_e
+Range = df['high'][split:] - df['low'][split:]
