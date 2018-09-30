dc8b51b391b2ed43e23f79b18b8f85428df3f661
Sealy <as70906n@pace.edu>
2018-09-30 12:42:38 -0400

    New additions for scaling data and setting restrictions.

--- trading_ml.py
+++ trading_ml.py
@@ -18,5 +18,25 @@ avg_train_err = {}
 df = web.DataReader('SPY', 'robinhood',start, end )
 df.head()
 
-df = df[['open_price', 'high_price', 'low_price', 'volume']]
+df = df[['open_price', 'high_price', 'low_price', 'close_price','volume']]
 df['open'] = df['open_price'].shift(1)
+df['high'] = df['high_price'].shift(1)
+df['low'] = df['low_price'].shift(1)
+df['close'] = df['close_price'].shift(1)
+df['volume'] = df['volume'].shift(1)
+
+X = df[['open', 'high', 'low', 'close']]
+y = df['close']
+
+imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
+
+steps = [('imputation', imp),
+         ('scaler', StandardScaler()),
+         ('lasso', Lasso())]
+
+pipeline = Pipeline(steps)
+
+parameters = {'lasso_alpha':np.arange(0.0001, 10,.0001),
+              'lasso_max_iter':np.random.uniform(100,100000,4)}
+
+reg = rcv(pipeline, parameters, cv=5)
