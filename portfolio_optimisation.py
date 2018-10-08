269b07731d6f0b936ad4bf181aa9de696c753955
Sealy <as70906n@pace.edu>
2018-10-08 14:24:40 -0400

    New code for portfolio optimization.

---
+++ portfolio_optimisation.py
@@ -0,0 +1,103 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import quandl
+import scipy.optimize as sco
+
+np.random.seed(123)
+
+%matplotlib inline
+%config InlineBackend.figure_format = 'retina'
+
+quandl.ApiConfig.api_key = 'asLyyb8z8JXQ35tC89RF'
+stocks = ['AAPL', 'AMZN', 'GOOGL', 'FB']
+data = quandl.get_table('WIKI/PRICES', ticker = stocks,
+                                       qopts = { 'columns': ['date', 'ticker', 'adj_close'] },
+                                       date = { 'gte': '2016-1-1', 'lte': '2018-09-31' }, paginate=True)
+
+data.head()
+data.info()
+
+df = data.set_index('date')
+table = df.pivot(columns = 'ticker')
+table.columns = [col[1] for col in table.columns]
+table.head()
+
+plt.figure(figsize=(14, 7))
+for c in table.columns.values:
+    plt.plot(table.index, table[c], lw=3, alpha=0.8,label=c)
+plt.legend(loc='upper left', fontsize=12)
+plt.ylabel('price in $')
+
+returns = table.pct_change()
+
+plt.figure(figsize=(14, 7))
+for c in returns.columns.values:
+    plt.plot(returns.index, returns[c], lw=3, alpha=0.8,label=c)
+plt.legend(loc='upper right', fontsize=12)
+plt.ylabel('daily returns')
+
+def portfolio_annualised_performance(weights, mean_returns, cov_matrix):
+    returns = np.sum(mean_returns*weights ) *252
+    std = np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights))) * np.sqrt(252)
+    return std, returns
+
+def random_portfolios(num_portfolios, mean_returns, cov_matrix, risk_free_rate):
+    results = np.zeros((3,num_portfolios))
+    weights_record = []
+    for i in range(num_portfolios):
+        weights = np.random.random(4)
+        weights /= np.sum(weights)
+        weights_record.append(weights)
+        portfolio_std_dev, portfolio_return = portfolio_annualised_performance(weights, mean_returns, cov_matrix)
+        results[0,i] = portfolio_std_dev
+        results[1,i] = portfolio_return
+        results[2,i] = (portfolio_return - risk_free_rate) / portfolio_std_dev
+    return results, weights_record
+
+returns = table.pct_change()
+mean_returns = returns.mean()
+cov_matrix = returns.cov()
+num_portfolios = 25000
+risk_free_rate = 0.0278
+
+def display_simulated_ef_with_random(mean_returns, cov_matrix, num_portfolios, risk_free_rate):
+    results, weights = random_portfolios(num_portfolios,mean_returns, cov_matrix, risk_free_rate)
+
+    max_sharpe_idx = np.argmax(results[2])
+    sdp, rp = results[0,max_sharpe_idx], results[1,max_sharpe_idx]
+    max_sharpe_allocation = pd.DataFrame(weights[max_sharpe_idx],index=table.columns,columns=['allocation'])
+    max_sharpe_allocation.allocation = [round(i*100,2)for i in max_sharpe_allocation.allocation]
+    max_sharpe_allocation = max_sharpe_allocation.T
+
+    min_vol_idx = np.argmin(results[0])
+    sdp_min, rp_min = results[0,min_vol_idx], results[1,min_vol_idx]
+    min_vol_allocation = pd.DataFrame(weights[min_vol_idx],index=table.columns,columns=['allocation'])
+    min_vol_allocation.allocation = [round(i*100,2)for i in min_vol_allocation.allocation]
+    min_vol_allocation = min_vol_allocation.T
+
+    print( "-"*80)
+    print( "Maximum Sharpe Ratio Portfolio Allocation\n")
+    print( "Annualized Return:", round(rp,2))
+    print( "Annualized Volatility:", round(sdp,2))
+    print( "\n")
+    print( max_sharpe_allocation)
+    print( "-"*80)
+    print( "Minimum Volatility Portfolio Allocation\n")
+    print( "Annualized Return:", round(rp_min,2))
+    print( "Annualized Volatility:", round(sdp_min,2))
+    print( "\n")
+    print( min_vol_allocation)
+
+    plt.figure(figsize=(10, 7))
+    plt.scatter(results[0,:],results[1,:],c=results[2,:],cmap='YlGnBu', marker='o', s=10, alpha=0.3)
+    plt.colorbar()
+    plt.scatter(sdp,rp,marker='*',color='r',s=500, label='Maximum Sharpe ratio')
+    plt.scatter(sdp_min,rp_min,marker='*',color='g',s=500, label='Minimum volatility')
+    plt.title('Simulated Portfolio Optimization based on Efficient Frontier')
+    plt.xlabel('Annualized Volatility')
+    plt.ylabel('Annualized Returns')
+    plt.legend(labelspacing=0.8)
+
+display_simulated_ef_with_random(mean_returns, cov_matrix, num_portfolios, risk_free_rate)
