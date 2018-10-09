f597feb74f30ca0b1ead5b8fe0ee48a91df73acd
Andre Sealy <andresealy@Andres-MacBook.local>
2018-10-09 07:14:24 -0400

    Working on new code.

--- Portfolio Management.py
+++ Portfolio Management.py
@@ -4,3 +4,22 @@ import datetime
 import matplotlib.pyplot as plt
 import plotly.graph_objs as go
 %matplotlib inline
+from pandas_datareader import data as pdr
+import fix_yahoo_finance as yf
+
+from plotly import __version__
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+print(__version__)
+
+yf.pdr_override()
+
+start_sp = datetime.datetime(2013,1,1)
+end_sp = datetime.datetime(2018,3,9)
+
+end_of_last_year = datetime.datetime(2017,12,29)
+
+stocks_start = datetime.datetime(2013,1,1)
+stocks_end = datetime.datetime(2018,3,9)
+
+sp500 = pdr.get_data_yahoo('^GSPC', start_sp, end_sp)
