4fe58630e32a318d18f004d6e50d35b98f44b819
Sealy <as70906n@pace.edu>
2018-10-15 10:26:37 -0400

    Finished module.

--- algo_trading/event.py
+++ algo_trading/event.py
@@ -81,3 +81,67 @@ class OrderEvent(Event):
             'Order: Symbol=%s, Type=%s, Quantity=%s, Direction=%s' %
             (self.symbol, self.order_type, self.quantity, self.direction)
         )
+
+class FillEvent(Event):
+    """
+    Encapsulates the notion of a Filled Order, as returned
+    from a brokerage. Stores the quantity of an instrument
+    actually filled and at what price. In addition, stores
+    the commission of the trade from the brokerage.
+
+    TODO: Currently does not support filling positions at
+    different prices. This will be simulated by averaging
+    the cost.
+    """
+
+
+    def __init__(self, timeindex, symbol, exchange, quantity,
+                 direction, fill_cost, commission=None):
+        """
+        Initalizes the FillEvent object. Sets the symbol, exchange, 
+        quantity, direction, cost of fill and an optional commission.
+
+        If commssion is not provided, the Fill object will calculate
+        it based on the trade size and Interactive Brokers fees.
+
+        Parameters:
+        timeindex - The bar-resolution when the order was filled.
+        symbol - The instrument which was filled
+        exchange - The exchange where the order was filled.
+        quantity - The filled quantity.
+        direction - The direction of fill ('BUY' or 'SELL')
+        fill_cost - The holdings value in dollars.
+        commission - An optional commission sent from IB.
+        """
+        self.type = 'FILL'
+        self.timeindex = timeindex
+        self.symbol = symbol
+        self.exchange = exchange
+        self.quantity = quantity
+        self.direction = direction
+
+        #Calculate commission
+        if commision is None:
+            self.commision = self.calculate_ib_commission()
+        else:
+            self.commission = commission
+
+    def calculate_ib_commission(self):
+        """
+        Calculates the fees of trading based on an Interactive
+        Brokers fee structure for API, in USD.
+
+        This does not include exchange or ECN fees.
+
+        Based on "US API Directed Orders":
+        htts://www.interactivebrokers.com/en/index.php?f=commission&p=stock2
+        """
+        full_cost = 1.3
+        if self.quantity <= 500:
+            full_cost = max(1.3, 0.013 * self.quantity)
+        else: #Greater than 500
+            full_cost = max(1.3, 0.008 * self.quantity)
+        return full_cost
+        
+
+                 
