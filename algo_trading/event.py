0929d1525fff42f0db9d60381587614508243155
Sealy <as70906n@pace.edu>
2018-10-15 09:38:21 -0400

    New code for creating an event module

---
+++ algo_trading/event.py
@@ -0,0 +1,56 @@
+from __future__ import print_function
+
+class Event(object):
+    """
+    Event is base class providing an interface for all subsequent
+    (inherited) events, that will trigger further events in the
+    trading infrastructure.
+    """
+    pass
+
+class MarketEvent(Event):
+    """
+    Handles the event of receiving a new market update with
+    corresponding bars.
+    """
+
+    def __init__(self):
+        """
+        Initializes the MarketEvent.
+        """
+        self.type = 'MARKET'
+
+class SignalEvent(Event):
+    """
+    Handles the event of sending a Signal from a Strategy object.
+    This is received by a Portfolio object and acted upon.
+    """
+
+    def __init__(self, strategy_id, symbol, datetime, signal_type, strength):
+        """
+        Initializes the SignalEvent.
+
+        Parameters:
+        strategy_id - The unique ID of the strategy sending the signal.
+        symbol - The ticker symbol, e.g. 'GOOG'.
+        datetime - The timestamp at which the signal was generated.
+        signal_type - 'LONG' or 'SHORT'
+        strength - An adjustment factor 'suggestion' used to scale
+            quantity at the portfolio level. Useful for pair strategies.
+        """
+        self.strategy_id = strategy_id
+        self.type = 'SIGNAL'
+        self.symbol = symbol
+        self.datetime = datetime
+        self.signal_type = signal_type
+        self.strength = strength
+
+class OrderEvent(Event):
+    """
+    Handles the event of sending an Order to an execution systemself.
+    The order contains a symbol (e.g. GOOG), a type (market or limit),
+    quantity and a direction.
+    """
+
+    def __init__(self, symbol, order_type, quantity, direction):
+        """
