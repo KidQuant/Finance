600ae59c96f38e308a52ea6621b43887544cbe38
Sealy <as70906n@pace.edu>
2018-10-15 09:50:04 -0400

    Order Event and Signal Event.

--- algo_trading/event.py
+++ algo_trading/event.py
@@ -54,3 +54,30 @@ class OrderEvent(Event):
 
     def __init__(self, symbol, order_type, quantity, direction):
         """
+        Initializes the order type, setting whether it is
+        a Market order ('MKT') or Limit order ('LMT'), has a
+        quantity (integral) and its direction ('BUY' or 'SELL').
+
+        TODO: Must handle error checking here to obtain
+        rational orders (i.e. no negative quantities etc).
+
+        Parameters:
+        symbol - The instrument to trade.
+        order_type - 'MKT' or 'LMT' for Market or Limit.
+        quantity - Non-negative integer for quantity.
+        direction - 'BUY' or 'Sell' for long or short.
+        """
+        self.type = 'ORDER'
+        self.symbol = symbol
+        self.order_type = order_type
+        self.quantity = quandtity
+        self.direction = direction
+
+    def print_order(self):
+        """
+        Outputs the values within the Order.
+        """
+        print(
+            'Order: Symbol=%s, Type=%s, Quantity=%s, Direction=%s' %
+            (self.symbol, self.order_type, self.quantity, self.direction)
+        )
