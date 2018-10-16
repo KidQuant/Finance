f81b42703971aa6ee9c6ea14974f0076475ea869
Andre Sealy <andresealy@Andres-MacBook.local>
2018-10-16 08:35:54 -0400

    New file for parsing data.

---
+++ algo_trading/data.py
@@ -0,0 +1,116 @@
+# data.py
+
+from __future__ import print_function
+
+from abc import ABCMeta, abstractmethod
+import datetime
+import os, os.path
+
+import numpy as np
+import pandas as pd
+
+from event import MarketEvent
+
+class DataHandler(object):
+    """
+    DataHandler is an abstract base class providing an interface for
+    all subsequent (inherited) data handlers (both live and historic).
+
+    The goal of a (derived) DataHandler object is to output a generated
+    set of bats (OHLCVI) for each symbol requested.
+
+    This will replicate how a live strategy would function as current
+    market data would be sent "down the pipe." Thus a historic and live
+    system will be treated identically by the rest of the backtesting suite.
+    """
+
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def get_latest_bar(self, symbol):
+        """
+        Returns the last bar updated.
+        """
+        raise NotImplementedError('Should implement get_latest_bar()')
+
+    @abstractmethod
+    def get_latest_bar(self, symbol, N=1):
+        """
+        Returns the last N bars updated.
+        """
+        raise NotImplementedError('Should implement get_latest_bar')
+
+    @abstractmethod
+    def get_latest_bar_datetime(self, symbol):
+        """
+        Returns a Python datetime object for the last bar.
+        """
+        raise NotImplementError('Should implement get_latest_bar_datetime()')
+
+    @abstractmethod
+    def get_latest_bar_value(self, symbol, val_type):
+        """
+        Returns on of the Open, High, Low, Close, Volume or OI
+        from the last bar.
+        """
+        raise NotImplementError('Should implement get_latest_bar_value()')
+
+    @abstractmethod
+    def get_latest_bar_values(self, symbol, val_type, N=1):
+        """
+        Returns the last N bar values from the latest_symbol
+        list, or N-k if less available.
+        """
+        raise NotImplementError('Should implement get_latest_bar_values()')
+
+    @abstractmethod
+    def update_bars(self):
+        """
+        Pushes the latest bars to the bars_queue for each symbol
+        in a tuple OHLCVI format: (datetime, open, high, low,
+        close, volume, open interest).
+        """
+        raise NotImplementedError('Should implement update_bars()')
+
+class HistoricCSVDataHandler(DataHandler):
+    """HistoricCSVDataHandler is designed to read CSV files for
+    each requested symbol from disk and provide an interface to obtain
+    'latest' bar in a manner identical to a live trading interface.
+    """
+
+    def __init__(self, events, csv_dir, symbol_list):
+        """
+        Initializes the historic data handler by requesting
+        the location of the CSV files and a list of symbols.
+
+        It will be assumed that all files are of the form
+        'symbol.csv,' where symbol is a string in the list.
+
+        Paramaters:
+        events - The Event Queue.
+        csv_dir - Absolute directory path to the CSV files.
+        symbol_list - A list of symbol strings.
+        """
+        self.events = events
+        self.csv_dir = csv_dir
+        self.symbol_list = symbol_list
+
+        self.symbol_data = {}
+        self.latest_symbol_data = {}
+        self.continue_backtest = True
+        self.bar_index = 0
+
+        self.open_convert_csv_files()
+
+    def _open_convert_csv_files(self):
+        """
+        Opens the CSV files from the data directory, converting
+        them into pandas DataFrames within a symbol dictionary.
+
+        For this handler it will be assumed that the data is
+        taken from Yahoo. Thus its dormat will be respected.
+        """
+        comb_index = None
+        for s in self.symbol_list:
+            #Load the CSV file with no header information, indexed on date
+            self.symbol_data[s] = pd.io.parsers.read_csv()
