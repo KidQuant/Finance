e1c870bcd90119c51b3a4467c7c8ee1c15627246
Sealy <as70906n@pace.edu>
2018-10-18 20:21:01 -0400

    Finished code.

--- algo_trading/iqfeed.py
+++ algo_trading/iqfeed.py
@@ -26,3 +26,31 @@ def read_historical_data_socket(sock, recv_buffer = 4096):
 
     buffer = buffer[:-12]
     return buffer
+
+if __name __ == '__main__':
+    #Define server host, port, and symbols to download
+    host = '127.0.0.1'
+    port = 9100 #Historical data support
+    syms = ['SPY', 'IWM']
+
+    #Download each symbol to disk
+    for sym in syms:
+        print('Downloading symbol: %s...' % sym)
+
+        # Construct the message needed by IQFeed to retrieve data
+        message = 'HIT,%s,60,20070101 075000,,,093000,160000,1\n' % sym
+
+        # Open a streaming socket to the IQFeed server locally
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((host, port))
+
+        # Send the historical data request
+        # message and buffer the data
+        sock.sendall(message)
+        data = read_historical_data_socket(sock)
+        sock.close
+
+        # Remove all the endlines and line-ending
+        # comma delimiter from each record
+        data = ''.join(data.split('\r'))
+        data = data.replace(',\n', '\n')[:-1]
