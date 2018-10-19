1ac1c8eb027c8d638a1c61f226176514ba689998
Sealy <as70906n@pace.edu>
2018-10-19 10:14:20 -0400

    New changes for the fix yahoo API

--- algo_trading/sharpe.py
+++ algo_trading/sharpe.py
@@ -7,6 +7,7 @@ import datetime
 import numpy as np
 import pandas as pd
 from pandas_datareader import data as web
+import fix_yahoo_finance
 
 def annualized_sharpe(returns, N=252):
     """
@@ -28,7 +29,23 @@ def equity_sharpe(ticker):
     The dates have been hardcoded here for brevity.
     """
 
-    start = datetime.datetime(2017,1,1)
+    start = datetime.datetime(2013,1,1)
     end = datetime.datetime.now()
 
-    
+    # Obtain the equities daily historic data for the desired time period
+    # and add to pandas DataFrame
+    pdf = web.get_data_yahoo(ticker, start, end)
+
+    # Use the percentage change method to calculate daily returns
+    pdf['daily_ret'] = pdf['Adj Close'].pct_change()
+
+    # Assume an average annual risk-free rate over the period of 5%
+    pdf['excess_daily_ret'] = pdf['daily_ret'] - 0.05 / 252
+
+    # Returns the annualized Sharpe ratio based on the excess daily returns
+    return annualized_sharpe(pdf['excess_daily_ret'])
+
+equity_sharpe('GOOGL')
+
+
+web.get_data_yahoo('AAPL', '2015-01-01', '2018-07-01')
