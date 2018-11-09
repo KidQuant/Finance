376fa9fc88020886530ca1fc6d4b403991421744
Sealy <as70906n@pace.edu>
2018-11-09 12:13:12 -0500

    New updates for quandl and forecasting.

--- algo_trading/forecast.py
+++ algo_trading/forecast.py
@@ -7,6 +7,7 @@ import datetime
 import numpy as np
 import pandas as pd
 import sklearn
+import fix_yahoo_finance as yf
 
 from pandas_datareader import data as pdr
 from sklearn.ensemble import RandomForestClassifier
@@ -15,3 +16,27 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.metrics import confusion_matrix
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.svm import LinearSVC, SVC
+
+yf.pdr_override()
+
+def create_lagged_seris(symbol, start_date, end_date, lags = 5):
+    """
+    This creates a pandas DataFrame that stores the
+    percentage returns of the adjusted closing value of
+    a stock obtained from Yahoo Finance, along with a
+    number of lagged returns from the prior trading days
+    (lags defaults to 5 days). Trading volume, as well as
+    the Direction from the previous day, are also included.
+    """
+
+    #Obtain stock information from Yahoo Finance
+
+    ts = pdr.get_data_yahoo(
+            symbol,
+            start_date-datetime.timedelta(days=365),
+            end_date
+    )
+
+    #Create the new lagged DataFrame
+
+    tslag = pd.DataFrame(index = ts.index)
