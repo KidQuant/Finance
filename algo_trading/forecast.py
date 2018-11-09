286a18efc64d8e76f37774fed2c7274b38c5d38a
Sealy <as70906n@pace.edu>
2018-11-09 12:27:59 -0500

    Finsihed creating lagged series

--- algo_trading/forecast.py
+++ algo_trading/forecast.py
@@ -19,7 +19,7 @@ from sklearn.svm import LinearSVC, SVC
 
 yf.pdr_override()
 
-def create_lagged_seris(symbol, start_date, end_date, lags = 5):
+def create_lagged_series(symbol, start_date, end_date, lags = 5):
     """
     This creates a pandas DataFrame that stores the
     percentage returns of the adjusted closing value of
@@ -40,3 +40,31 @@ def create_lagged_seris(symbol, start_date, end_date, lags = 5):
     #Create the new lagged DataFrame
 
     tslag = pd.DataFrame(index = ts.index)
+    tslag['Today'] = ts['Adj Close']
+    tslag['Volume'] = ts['Volume']
+
+    # Create the shifted lag series of prior trading period close values
+    for i in range(0, lags):
+        tslag['lag%s' % str(i+1)] = ts['Adj Close'].shift(i+1)
+
+    # Create the return DataFrame
+    tsret = pd.DataFrame(index = tslag.index)
+    tsret['Volume'] = tslag['Volume']
+    tsret['Today'] = tslag['Today'].pct_change() * 100.0
+
+    #If any of the values of percentage return equal zero, set them to
+    # a small number (stops issues with QDA model in scikit-learn)
+    for i, x enumerate(tsret['Today']):
+        if (abs(x) < 0.0001):
+            tsret['Today'][i] = 0.0001
+
+    # Create the lagged percentage returns column
+    for i in range(0, lags):
+        tsret['Lag%s' % str(i+1)] = \
+        tslag['Lag%s' % str(i+1)].pct_change()*100.0
+
+    # Create the "Direction" column (+1 of -1) indicating an up/down day
+    tsret['Direction'] = np.sign(tsret['Today'])
+    tsret = tsret[tsret.index >= start_date]
+
+    return tsret
