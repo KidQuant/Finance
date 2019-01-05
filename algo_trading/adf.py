0ea28bfd2e41812398039a74c0bc443edebbb128
Andre Sealy <andre@hedgology.com>
2019-01-05 15:31:07 -0500

    New additions for hurst exponential function

--- algo_trading/adf.py
+++ algo_trading/adf.py
@@ -14,3 +14,42 @@ end = dt.datetime.now()
 amzn = pdr.get_data_yahoo('AMZN', start, end)['Adj Close']
 
 ts.adfuller(amzn)
+
+from numpy import cumsum, log, polyfit, sqrt, std, subtract
+from numpy.random import randn
+
+def hurst(ts):
+    """Returns the Hurst Exponent of the time series vector ts"""
+    #Create the range of lag values
+    lags = range(2, 100)
+
+    #Calculate the array of the variances of the lagged differences
+    tau = [sqrt(std(subtract(ts[lag:], ts[:-lag]))) for lag in lags]
+
+    #Use a linear fit to estimate the Hurst Exponent
+    poly = polyfit(log(lags), log(tau), 1)
+
+    # Return the Hurst exponent from the polyfit output
+    return poly[0]*2.0
+
+#Create a Gometric Brownian Motion, Mean-Reverting and Trending Series
+gbm = log(cumsum(randn(100000))+1000)
+mr = log(randn(100000)+1000)
+tr = log(cumsum(randn(100000)+1)+1000)
+
+#Output the Hurst Exponent for each of the above series
+# and the price of Amazon (the Adjusted Close price) for
+# the ADF test given above in the article
+print("Hurst(GBM):      %s"  % hurst(gbm))
+print("Hurt(MR):        %s"  % hurst(mr))
+print("Hurst(TR):       %s"  % hurst(tr))
+
+#Assuming you have run the above code for Amazon
+
+print("Hurst(AMZN)      %s"  % hurst(amzn))
+
+
+hurst(gbm)
+hurst(mr)
+hurst(tr)
+hurst(amzn)
