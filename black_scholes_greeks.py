536f64a50ece412c14395d77b858ceaf8e8c35a1
Hedgology <as70906n@pace.edu>
2019-03-14 23:24:15 -0400

    Updated code for the greeks

--- black_scholes_greeks.py
+++ black_scholes_greeks.py
@@ -233,11 +233,74 @@ def d1_calc(S,K,r,vol,T,t):
     #Calculates d1 in the BSM equation
     return (np.log(S/K) + (r + 0.5 * vol**2) * (T-t)/(vol*np.sqrt(T-t)))
 
-def BS_call(S,K,r,vol,T,t):
-    d1 = d1_calc(S,K,r,vol,T,t)
+def BS_call(S, K, r, vol, T, t):
+    d1 = d1_calc(S, K, r, vol, T, t)
     d2 = d1 - vol * np.sqrt(T)
     return S * norm.cdf(d1) - K * np.exp(-r*T)*norm.cdf(d2)
 
-def BS_put(S,K,r,vol,T,t):
-    return BS_call(S,K,r,vol,T,t) - S + np.exp(-r*(T-t))*K
+def BS_put(S, K, r, vol, T, t):
+    return BS_call(S, K, r, vol, T, t) - S + np.exp(-r*(T-t))*K
 
+def BS_binary_call(S, K, r, vol, T, t):
+    d1 = d1_calc(S, K, r, vol, T, t)
+    d2 = d1 - vol * np.sqrt(T-t)
+    return np.exp(-r*T)*norm.cdf(d2)
+
+def BS_binary_put(S, K, r, vol, T, t):
+    return BS_binary_call(S, K, r, vol, T,t) - S + np.exp(-r*(T-t))*K
+
+#1st Order Greeks
+
+def delta(S, K, r, vol, T, t, otype):
+    d1 = d1_calc(S, K, r, vol, T, t)
+    d2 = d1 - vol * np.sqrt(T-t)
+    
+    if(otype == "call"):
+        delta = np.exp(-(T-t))*norm.cdf(d1)
+    elif(otype == "put"):
+        delta = -np.exp(-(T-t))*norm.cdf(-d1)
+
+    return delta
+
+#Gamma for calls/puts the same
+
+def vega(S,K,r,vol,T,t,otype):
+    d1 = d1_calc(S,K,r,vol,T,t)
+    return S * norm.pdf(d1) * np.sqrt(T-t)
+
+def rho(S,K,r,vol,T,t,otype):
+    d1 = d1_calc(S,K,r,vol,T,t)
+    d2 = d1 - vol*np.sqrt(T-t)
+
+    if(otype == 'call'):
+        rho = K*(T-t)*np.exp(-r*(T-t))*norm.cdf(d2)
+    elif(otype == 'put'):
+        rho = -K*(T-t)*np.exp(-r*(T-t))*norm.cdf(-d2)
+
+    return rho
+
+def theta(S, K, r, vol, T, t, otype):
+    d1 = d1_calc(S, K, r, vol, T, t)
+    d2 = d1 - vol*np.sqrt(T-t)
+
+    if(otype == 'call'):
+        theta = -(S*norm.pdf(d1)*vol / (2*np.sqrt(T-t))) - r*K*np.exp(-r*(T-t))*norm.cdf(d2)
+    elif(otype == 'put'):
+        theta = -(S*norm.pdf(d1)*vol / (2*np.sqrt(T-t))) + r*K*np.exp(-r*(T-t))*norm.cdf(-d2)
+
+    return theta
+
+#2nd Order Greeks
+
+def gamma(S, K, r, vol, T, t, otype):
+    d1 = d1_calc(S, K, r, vol, T, t)
+    gamma = (norm.pdf(d1)) / (S * vol * np.sqrt(T-t))
+
+    return gamma
+
+def charm(S , K, r, vol, T, t, otype):
+    d1 = d1_calc(S, K, r, vol, T, t)
+    d2 = d1 - vol*np.sqrt(T-t)
+    charm = -norm.pdf(d1)*(2*r*(T-t) - d2*vol*np.sqrt(T-t)/(2*(T-t)*vol*np.sqrt(T-t))) 
+
+    return charm
